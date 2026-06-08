ac33176bf6b5ff8be629b2c64dc45814a793363d
okkidididjdj <markkozarevych@gmail.com>
2026-06-08 22:57:21 +0300

    Refactor navigation controller initialization in AppDelegate

--- Applications/AppDelegate.m
+++ Applications/AppDelegate.m
@@ -1,20 +1,20 @@
-#import "AppDelegate.h"
+#import <UIKit/UIKit.h>
 #import "AuthViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
-    // Создаем твой экран авторизации
     AuthViewController *authVC = [[AuthViewController alloc] init];
-    
-    // Оборачиваем его в Navigation Controller, чтобы были красивые переходы
-    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:authVC];
-    
-    self.window.rootViewController = navController;
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:authVC];
+    self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
     return YES;
 }
 
 @end
+
